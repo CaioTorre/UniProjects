0a5b56a54372afda2bd25f2f9a5165d986a79091
Caio Torre <caiosanches@live.com>
2017-03-27 16:26:25 -0300

    Imma break this thing soon enough
    Wish me luck

--- diasDeVida.cpp
+++ diasDeVida.cpp
@@ -2,84 +2,97 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
 int main() {
 	int diaA, mesA, anoA, diaR, mesR, anoR, max, fail;
-	
+
 	diaA = 27;
 	mesA = 3;
 	anoA = 2017;
-	
+
 	fail = 0;
-	
+
+	//Receber datas para calculo
 	printf("Dia de Nascimento: ");
-	scanf("%i", &diaR);
+	scanf("%d", &diaR);
 	printf("Mes de Nascimento: ");
-	scanf("%i", &mesR);
+	scanf("%d", &mesR);
 	printf("Ano de Nascimento: ");
-	scanf("%i", &anoR);
+	scanf("%d", &anoR);
 	printf("------------------\n");
 	printf("Dia Atual: ");
-	scanf("%i", &diaA);
+	scanf("%d", &diaA);
 	printf("Mes Atual: ");
-	scanf("%i", &mesA);
+	scanf("%d", &mesA);
 	printf("Ano Atual: ");
-	scanf("%i", &anoA);
-	
-	if (diaR <= 0 || mesR <= 0 || anoR <= 0) {fail = 1;}
-	
+	scanf("%d", &anoA);
+
+	//Validar data de ANIVERSARIO
+	//Nenhum valor negativo
+	if (diaR <= 0 || mesR <= 0 || anoR <= 0) { fail = 1; }
+
+	//Se for o mesmo ano, o mes nao pode ser anterior
 	if (anoR == anoA) {
-	    	if (mesR > mesA) {fail = 1;}
-	    	if (mesR == mesA && diaR > diaA) {fail = 1;}
+		if (mesR > mesA) { fail = 1; }
+		//Se for o mesmo ano e o mesmo mes, o dia nao pode ser anterior
+		if (mesR == mesA && diaR > diaA) { fail = 1; }
 	}
-	if (mesR == 1 || mesR == 3 || mesR == 5 || mesR == 7 || mesR == 8 || mesR == 10 || mesR == 12) {
-		max = 31;
-	} else {
-		if (mesR == 4 || mesR == 6 || mesR == 9 || mesR == 11) {
-			max = 30;
-		} else {
-			if (mesR == 2) {
-				if ((anoR % 4 == 0 && anoR % 100 != 0) || anoR % 400 == 0) {
-					max = 29;
-				} else {
-					max = 28;
-				}
-			} else {
-				fail = 1;
-			}
-		}
-	}
-		
-	if (diaR > max) {fail = 1;}
+
+	//Definicao de dias maximos
+	if (mesR == 1 || mesR == 3 || mesR == 5 || mesR == 7 || mesR == 8 || mesR == 10 || mesR == 12) { max = 31; }
+	else if (mesR == 4 || mesR == 6 || mesR == 9 || mesR == 11) { max = 30; }
+	else if (mesR == 2) {
+		//Se for bissexto, a maxima para fevereiro é 29
+		if ((anoR % 4 == 0 && anoR % 100 != 0) || anoR % 400 == 0) { max = 29; }
+		else { max = 28; }
+	} else { fail = 1; } //Mes inexistente
 	
-	if (fail == 0) {
-	    
-		int temp1A = (mesA + 9)/12;
-		int temp2A = (7 * (anoA + temp1A))/4;
-		int temp3A = (275 * mesA)/9;
+	
+	//Dia nao pode ser maior que o maximo para aquele mes
+	if (diaR > max) { fail = 1; }
+
+	//Validar data ATUAL
+	//Nenhum valor negativo
+	if (diaA <= 0 || mesA <= 0 || anoA <= 0) { fail = 1; }
+
+	//Definicao de dias maximos
+	if (mesA == 1 || mesA == 3 || mesA == 5 || mesA == 7 || mesA == 8 || mesA == 10 || mesA == 12) { max = 31; }
+	else if (mesA == 4 || mesA == 6 || mesA == 9 || mesA == 11) { max = 30; }
+	else if (mesA == 2) {
+		//Se for bissexto, a maxima para fevereiro é 29
+		if ((anoA % 4 == 0 && anoA % 100 != 0) || anoA % 400 == 0) { max = 29; }
+		else { max = 28; }
+	}
+	else { fail = 1; } //Mes inexistente
+
+	//Dia nao pode ser maior que o maximo para aquele mes
+	if (diaA > max) { fail = 1; }
+
+	printf("------------------\n");
+
+	if (fail == 0) { //Se não falhar
+		//Calcular o equivalente em dias Julianos da data atual (http://aa.usno.navy.mil/faq/docs/JD_Formula.php)
+		int temp1A = (mesA + 9) / 12;
+		int temp2A = (7 * (anoA + temp1A)) / 4;
+		int temp3A = (275 * mesA) / 9;
 		int temp4A;
-		if ((100 * anoA + mesA - 190002.5) > 0) {
-			temp4A = 1;
-		} else {
-			temp4A = -1;
-		}
+		if ((100 * anoA + mesA - 190002.5) > 0) { temp4A = 1; } else { temp4A = -1; }
 		double julianoA = 367 * anoA - temp2A + temp3A + diaA + 1721013.5 - (0.5 * temp4A) + 0.5;
-		
-		int temp1R = (mesR + 9)/12;
-		int temp2R = (7 * (anoR + temp1R))/4;
-		int temp3R = (275 * mesR)/9;
+
+		//Calcular o equivalente em dias Julianos da data do aniversario
+		int temp1R = (mesR + 9) / 12;
+		int temp2R = (7 * (anoR + temp1R)) / 4;
+		int temp3R = (275 * mesR) / 9;
 		int temp4R;
-		if ((100 * anoR + mesR - 190002.5) > 0) {
-			temp4R = 1;
-		} else {
-			temp4R = -1;
-		}
+		if ((100 * anoR + mesR - 190002.5) > 0) { temp4R = 1; } else { temp4R = -1; }
 		double julianoR = 367 * anoR - temp2R + temp3R + diaR + 1721013.5 - (0.5 * temp4R) + 0.5;
-		
+
+		//Calcular a diferença das datas
 		int diff = julianoA - julianoR - 1;
 
-		printf("Voce tem %i dias de vida!", diff);
-	} else {
-		printf("Data invalida!\n");
-	}
+		printf("Voce tem %i dias de vida! (+1 com hoje)\n", diff);
+	} else { printf("Data invalida!\n"); }
+
+	system("pause");
 	return 0;
 }
